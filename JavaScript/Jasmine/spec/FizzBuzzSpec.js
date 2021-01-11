b82d151a2d29ed5842f0bc489fb487b000c38716
xavierloos <xavier.loos04@gmail.com>
2021-01-11 21:20:19 +0000

    is NOT divisible by 3

--- JavaScript/Jasmine/spec/FizzBuzzSpec.js
+++ JavaScript/Jasmine/spec/FizzBuzzSpec.js
@@ -6,4 +6,11 @@ describe("FizzBuzz", function () {
             expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
         });
     })
+
+    describe("knows when an number is NOT", function () {
+        it("divisible by 3", function () {
+            fizzbuzz = new FizzBuzz();
+            expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+        });
+    })
 })
