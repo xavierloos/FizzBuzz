097a5da8b16e24d217531fca81965e8b92fc4597
xavierloos <xavier.loos04@gmail.com>
2021-01-11 21:29:29 +0000

    Refactoring code using beforeEach

--- JavaScript/Jasmine/spec/FizzBuzzSpec.js
+++ JavaScript/Jasmine/spec/FizzBuzzSpec.js
@@ -1,39 +1,36 @@
 describe("FizzBuzz", function () {
     var fizzbuzz;
+    beforeEach(function () {
+        fizzbuzz = new FizzBuzz();
+    })
     describe("knows when an number is", function () {
         it("divisible by 3", function () {
-            fizzbuzz = new FizzBuzz();
             expect(fizzbuzz.isDivisibleBy3(3)).toBe(true);
         });
     })
 
     describe("knows when an number is NOT", function () {
         it("divisible by 3", function () {
-            fizzbuzz = new FizzBuzz();
             expect(fizzbuzz.isDivisibleBy3(1)).toBe(false);
         });
     })
     describe("knows when an number is", function () {
         it("divisible by 5", function () {
-            fizzbuzz = new FizzBuzz();
             expect(fizzbuzz.isDivisibleBy5(5)).toBe(true);
         });
     })
     describe("knows when an number is NOT", function () {
         it("divisible by 5", function () {
-            fizzbuzz = new FizzBuzz();
             expect(fizzbuzz.isDivisibleBy5(1)).toBe(false);
         });
     })
     describe("knows when an number is", function () {
         it("divisible by 3 & 5", function () {
-            fizzbuzz = new FizzBuzz();
             expect(fizzbuzz.isDivisibleBy15(15)).toBe(true);
         });
     })
     describe("knows when an number is NOT", function () {
         it("divisible by 3 & 5", function () {
-            fizzbuzz = new FizzBuzz();
             expect(fizzbuzz.isDivisibleBy15(11)).toBe(false);
         });
     })
