46cfc40a63eee9b661c570c7b1915addaff73154
xavierloos <xavier.loos04@gmail.com>
2021-01-11 21:47:48 +0000

    Refactoring Jasmine

--- JavaScript/Jasmine/spec/FizzBuzzSpec.js
+++ JavaScript/Jasmine/spec/FizzBuzzSpec.js
@@ -3,33 +3,26 @@ describe("FizzBuzz", function () {
     beforeEach(function () {
         fizzbuzz = new FizzBuzz();
     })
+    // TRUTHY
     describe("knows when an number is", function () {
         it("divisible by 3", function () {
             expect(fizzbuzz.isDivisibleBy3(3)).toBe(true);
         });
-    })
-
-    describe("knows when an number is NOT", function () {
-        it("divisible by 3", function () {
-            expect(fizzbuzz.isDivisibleBy3(1)).toBe(false);
-        });
-    })
-    describe("knows when an number is", function () {
         it("divisible by 5", function () {
             expect(fizzbuzz.isDivisibleBy5(5)).toBe(true);
         });
-    })
-    describe("knows when an number is NOT", function () {
-        it("divisible by 5", function () {
-            expect(fizzbuzz.isDivisibleBy5(1)).toBe(false);
-        });
-    })
-    describe("knows when an number is", function () {
         it("divisible by 3 & 5", function () {
             expect(fizzbuzz.isDivisibleBy15(15)).toBe(true);
         });
     })
+    // FALSY
     describe("knows when an number is NOT", function () {
+        it("divisible by 3", function () {
+            expect(fizzbuzz.isDivisibleBy3(1)).toBe(false);
+        });
+        it("divisible by 5", function () {
+            expect(fizzbuzz.isDivisibleBy5(1)).toBe(false);
+        });
         it("divisible by 3 & 5", function () {
             expect(fizzbuzz.isDivisibleBy15(11)).toBe(false);
         });
@@ -38,18 +31,12 @@ describe("FizzBuzz", function () {
         it("FIZZ if divisible by 3", function () {
             expect(fizzbuzz.says(3)).toEqual("FIZZ");
         });
-    })
-    describe("when playing, says", function () {
         it("BUZZ if divisible by 5", function () {
             expect(fizzbuzz.says(5)).toEqual("BUZZ");
         });
-    })
-    describe("when playing, says", function () {
         it("FIZZBUZZ if divisible by 15", function () {
             expect(fizzbuzz.says(15)).toEqual("FIZZBUZZ");
         });
-    })
-    describe("when playing, says", function () {
         it("number unless divisible by 3,5 or 15", function () {
             expect(fizzbuzz.says(11)).toEqual(11);
         });
--- JavaScript/Jasmine/src/FizzBuzz.js
+++ JavaScript/Jasmine/src/FizzBuzz.js
@@ -19,4 +19,5 @@ class FizzBuzz {
             return number
         }
     }
+
 }
